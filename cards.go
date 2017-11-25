c8ac90b5023f53fb23117a94a22698d95d31bb77
Andrew Siegman <blerrnk@gmail.com>
2017-11-25 16:04:06 -0600

    add card glyphs, add shuffle method

--- cards.go
+++ cards.go
@@ -1,23 +1,48 @@
 package deck
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Card struct {
-	Name, Suit string
-	Value      int
+	Name, Suit, Glyph string
+	Value             int
 }
 
-type Deck []Card
+type Deck struct {
+	Cards []Card
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 func NewDeck() Deck {
 	var d []Card
 	for _, s := range suits {
 		for v := 0; v < 13; v++ {
+			n := names[v]
+			full := n + " of " + s
+			g := glyphs[full]
+
 			c := Card{
-				Name:  names[v],
+				Name:  n,
 				Suit:  s,
 				Value: v + 1,
+				Glyph: g,
 			}
+
 			d = append(d, c)
 		}
 	}
-	return d
+	return Deck{Cards: d}
+}
+
+func (d *Deck) Shuffle() {
+	c := d.Cards
+	for i := range c {
+		j := rand.Intn(i + 1)
+		c[i], c[j] = c[j], c[i]
+	}
 }
