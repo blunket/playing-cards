77636bc5b45468e8bc5794c78f26d3b2556f687d
Andrew Siegman <blerrnk@gmail.com>
2017-11-25 17:12:14 -0600

    add Hand type

--- cards.go
+++ cards.go
@@ -2,15 +2,20 @@ package deck
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
 
 type Card struct {
-	Name, Suit, Glyph string
-	Value             int
+	Name, Face, Suit, Glyph string
+	Value                   int
 }
 
 type Deck struct {
+	Cards []Card
+}
+
+type Hand struct {
 	Cards []Card
 }
 
@@ -18,16 +23,19 @@ func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
+// NewDeck creates a deck of unshuffled cards.
 func NewDeck() Deck {
+	// d := make([]Card, 52)
 	var d []Card
 	for _, s := range suits {
 		for v := 0; v < 13; v++ {
-			n := names[v]
-			full := n + " of " + s
-			g := glyphs[full]
+			f := names[v]
+			n := f + " of " + s
+			g := glyphs[n]
 
 			c := Card{
 				Name:  n,
+				Face:  f,
 				Suit:  s,
 				Value: v + 1,
 				Glyph: g,
@@ -45,4 +53,28 @@ func (d *Deck) Shuffle() {
 		j := rand.Intn(i + 1)
 		c[i], c[j] = c[j], c[i]
 	}
+}
+
+func NewHand() Hand {
+	return Hand{}
+}
+
+func (h *Hand) Draw(d *Deck, n int) {
+	h.Cards, d.Cards = d.Cards[0:n], d.Cards[n+1:]
+}
+
+func (h Hand) String() string {
+	var g []string
+	for _, c := range h.Cards {
+		g = append(g, c.Glyph)
+	}
+	return strings.Join(g, " ")
+}
+
+func (d Deck) String() string {
+	var g []string
+	for _, c := range d.Cards {
+		g = append(g, c.Glyph)
+	}
+	return strings.Join(g, " ")
 }
